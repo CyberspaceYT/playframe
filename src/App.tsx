682aa98b256cd1aa6d8a82699e1e4782fa8f1ff3
Cyberspace110 <wyattwhittington@gmail.com>
2026-05-14 20:16:21 -0500

    Change GamePlayer import to named import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import GamePlayer from "./pages/GamePlayer";
+import { GamePlayer } from "./pages/GamePlayer";
 import Categories from "./pages/Categories";
 import Create from "./pages/Create";
 import NotFound from "./pages/NotFound";

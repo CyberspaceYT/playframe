7bc083ff96f28e71389c4d611c8b6a4247ace873
Cyberspace110 <wyattwhittington@gmail.com>
2026-05-14 20:24:23 -0500

    Remove GamePlayer route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import { GamePlayer } from "./pages/GamePlayer";
 import Categories from "./pages/Categories";
 import Create from "./pages/Create";
 import NotFound from "./pages/NotFound";
@@ -19,7 +18,6 @@ const queryClient = new QueryClient();
 const AppContent = () => {
   useTabVisibility();
   const [adminOpen, setAdminOpen] = useState(false);
-
   useAdminShortcut(() => setAdminOpen(true));
 
   return (
@@ -32,7 +30,6 @@ const AppContent = () => {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
-              <Route path="/play/:id" element={<GamePlayer />} />
               <Route path="/categories" element={<Categories />} />
               <Route path="/create" element={<Create />} />
               <Route path="*" element={<NotFound />} />

2f75e085975f53819eae74eb64621b16f78d5a36
Cyberspace110 <wyattwhittington@gmail.com>
2026-05-16 19:01:50 -0500

    Update App import to remove .tsx extension
    Removed .tsx extension from App import in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css' // Remove this line if you don't have index.css
+import App from './App'           // ← Remove .tsx
+import './index.css'              // Remove this if you get error here too
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
